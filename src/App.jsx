00ecc9a1379f430ed04d6a5560fc63dec568ca6c
geiledotum <geile.giovana@credip.com.br>
2023-06-15 17:59:37 -0400

    feat: inicia desenvolvimento de ideias

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import "./styles/global.css"
+import { Card } from "./Card"
+import { HandWaving } from "@phosphor-icons/react"
 
 function App() {
   return (
     <main className="flex flex-col p-7 bg-blue-900 min-w-screen min-h-screen h-full">
       
       <header>
-        <h1 className="text-3xl font-bold text-gray-200">Olá, bem vindo</h1>
-      </header>
-      <body className="flex">
-        <div className="w-40">
-          <img className="rounded-3xl border-solid border-4 border-green-700" src="https://github.com/giovanageile.png" alt="" />
+        <div className="flex gap-3">
+          <h1 className="text-3xl pb-2 font-bold text-gray-200">Olá, bem vindo</h1>
+          <HandWaving size={32} color="rgb(229 231 235)"/>
         </div>
+        <hr />
+      </header>
+      <body className="flex pt-3">
+       <Card />
+       <div className="w-full"></div>
+        {/* <div className="w-40">
+          <img className="rounded-3xl border-solid border-4 border-green-700" src="https://github.com/giovanageile.png" alt="" />
+        </div> */}
 
       </body>
 
---
+++ src/Card.jsx
@@ -0,0 +1,9 @@
+
+export function Card() {
+  return (
+    <div className="w-full h-80 border-2 border-solid border-slate-300 rounded-2xl ">
+      <h2>blah blah blah</h2>
+      <hr />
+    </div>
+  )
+}
